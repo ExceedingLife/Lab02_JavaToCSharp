df5da8a02c4978d457c1aa12a215a787cd5ff7fc
Andrew Harkins <extremelyexceeding@gmail.com>
2018-12-17 20:32:24 -0600

    Lab02 Making a Class and a Main
    RetailItem class - 3 fields, Getter methods, Setter methods, Constructors.
    
    RetailItemDemo - Main using RetailItem class, Using methods to change item fields.
    Reading in console application

--- Lab02_JavaToCSharp/Program.cs
+++ Lab02_JavaToCSharp/Program.cs
@@ -8,8 +8,36 @@ namespace Lab02_JavaToCSharp
 {
     class Program
     {
+        //Class RetailItemDemo but using the 'static void Main(string[] args)'
         static void Main(string[] args)
         {
+            // Create Item #1
+            RetailItem item1 = new RetailItem("Jacket", 12, 59.95);
+            // Create Item #2
+            RetailItem item2 = new RetailItem("Designer Jeans", 40, 34.95);
+            // Create Item #3
+            RetailItem item3 = new RetailItem("Shirt", 20, 24.95);
+
+            // Read Item1 & Item2 & Item3
+            Console.WriteLine("Item1 - Description: " + item1.GetDescription() + " - UnitsOnHand: " + item1.GetUnitsOnHand() + " - Price: " + item1.GetPrice());
+            Console.WriteLine("Item2 - Description: " + item2.GetDescription() + " - UnitsOnHand: " + item2.GetUnitsOnHand() + " - Price: " + item2.GetPrice());
+            Console.WriteLine("Item3 - Description: " + item3.GetDescription() + " - UnitsOnHand: " + item3.GetUnitsOnHand() + " - Price: " + item3.GetPrice());
+            Console.WriteLine();
+            Console.WriteLine("Before changing values on item1 (Using SetMethods)");
+            Console.WriteLine("Item 1 - Description: " + item1.GetDescription() + " - UnitsOnHand: " + item1.GetUnitsOnHand() + " - Price: " + item1.GetPrice());
+            // Use Mutator methods on Item1 to change the values.
+            Console.WriteLine();
+            Console.WriteLine("After changing values on item1 (Using SetMethods)");
+            Console.WriteLine();
+            item1.SetDescription("Brewers Hat");
+            Console.WriteLine("item1.SetDescription('Brewers Hat')");
+            item1.SetUnitsOnHand(5);
+            Console.WriteLine("item1.SetUnitsOnHand(5)");
+            item1.SetPrice(49.99);
+            Console.WriteLine("item1.SetPrice(49.99)");
+            Console.WriteLine();
+            Console.WriteLine("Item1 - Description: " + item1.GetDescription() + " - UnitsOnHand: " + item1.GetUnitsOnHand() + " - Price: " + item1.GetPrice());
+            Console.ReadLine();
         }
     }
 }
---
+++ Lab02_JavaToCSharp/RetailItem.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab02_JavaToCSharp
+{
+    class RetailItem
+    {
+        //Fields
+        private string description = "";
+        private int unitsOnHand = 0;
+        private double price = 0;
+
+        //Constructors
+        public RetailItem() { }
+
+        public RetailItem(string desc, int units, double pric)
+        {
+            description = desc;
+            unitsOnHand = units;
+            price = pric;
+        }
+
+        // Mutators in Java are SetMethods().
+        // Accessors in Java are GetMethods().
+
+        public void SetDescription(string d)
+        {
+            description = d;
+        }
+        public string GetDescription()
+        {
+            return description;
+        }
+
+        public void SetUnitsOnHand(int u)
+        {
+            unitsOnHand = u;
+        }
+        public int GetUnitsOnHand()
+        {
+            return unitsOnHand;
+        }
+
+        public void SetPrice(double p)
+        {
+            price = p;
+        }
+        public double GetPrice()
+        {
+            return price;
+        }
+    }
+}
